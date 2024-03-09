3a0a53796b0c7e834e3b87acbb64677c5b4f4201
Kalimullin Marat <mer-marat@yandex.ru>
2024-03-09 13:24:57 +0300

    GOHW-1: Add file help.go with printing usage information and delete func help() from main

--- Homework-1/cmd/main.go
+++ Homework-1/cmd/main.go
@@ -9,35 +9,6 @@ import (
 	"strconv"
 )
 
-// help prints usage guide
-func help() {
-	fmt.Println(`
-	usage: go run ./cmd -command=<help|get|remove|give|list|return|list-return> [-id=<order id>] [-recipient=<recipient id>] [-expire=<expire date>] [-t=<bool>] [<args>]
-
-	Command desciption:
-		help: список доступных команд с кратким описанием
-		get: принять заказ от курьера
-		remove: вернуть заказ курьеру
-		give: выдать заказ клиенту
-		list: получить список заказов
-		return: принять возврат от клиента
-		list-return: получить список возвратов
-
-	Needed flags or arguments for each command:
-		help	
-		get 		 -id -recipient -expire
-		remove  	 -id
-		give		 args: order ids to give (example: go run ./cmd -command=give 1 2 3 4)
-		list		 -recipient (optional flag -t: boolean value for printing orders located in our point (not already given); optional args: number of orders to list or zero fo all)
-		return  	 -id -recipient
-		list-return	 args: page number and number of orders per page (default: all pages and 10 orders per page) (example: "-command=list-return 2 5" prints 2nd page of returned orders grouped by 5 orders in each page)
-	
-	Flags requirements:
-		-id, -recipient: positive number
-		-expire: date in 'dd.mm.yyyy' format (02.01.2006 for 2nd Jan 2006)
-	`)
-}
-
 func main() {
 	command := flag.String("command", "", "name of command")
 	id := flag.Int("id", 0, "order id")
