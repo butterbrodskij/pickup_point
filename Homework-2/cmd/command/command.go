673d909f7b9f845d8c1b076a88fc65edc99d3a06
Kalimullin Marat <mer-marat@yandex.ru>
2024-03-13 19:37:50 +0300

    GOHW-2: Replace implementation of commands from main to package command

--- Homework-2/cmd/command/command.go
+++ Homework-2/cmd/command/command.go
@@ -1,6 +1,12 @@
 package command
 
-import "fmt"
+import (
+	"fmt"
+	"homework2/pup/cmd/parsing"
+	"homework2/pup/internal/model"
+	"homework2/pup/internal/service"
+	"strconv"
+)
 
 // help prints usage guide
 func Help() {
@@ -29,4 +35,134 @@ func Help() {
 		-id, -recipient: positive number
 		-expire: date in 'dd.mm.yyyy' format (02.01.2006 for 2nd Jan 2006)
 	`)
+}
+
+func Accept(serv service.Service, params parsing.Params) {
+	if params.ID == nil || params.RecipientID == nil || params.ExpireString == nil {
+		fmt.Println("miss required flags")
+		return
+	}
+	err := serv.AcceptFromCourier(model.OrderInput{
+		ID:          *params.ID,
+		RecipientID: *params.RecipientID,
+		ExpireDate:  *params.ExpireString,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("got new order from courier")
+}
+
+func Remove(serv service.Service, params parsing.Params) {
+	if params.ID == nil || params.RecipientID == nil || params.ExpireString == nil {
+		fmt.Println("miss required flags")
+		return
+	}
+	err := serv.Remove(*params.ID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("removed order %d from our pick-up point\n", *params.ID)
+}
+
+func Give(serv service.Service, params parsing.Params) {
+	if len(params.Args) == 0 {
+		fmt.Println("expected at least one argument as order id")
+		return
+	}
+	ids := make([]int64, len(params.Args))
+	for i, s := range params.Args {
+		idCur, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		ids[i] = idCur
+	}
+	err := serv.Give(ids)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("all orders have been given to its recipient")
+}
+
+func List(serv service.Service, params parsing.Params) {
+	if params.RecipientID == nil {
+		fmt.Println("miss required flags")
+		return
+	}
+	var (
+		n   int
+		err error
+	)
+	if len(params.Args) > 0 {
+		n, err = strconv.Atoi(params.Args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	foundArr, err := serv.List(*params.RecipientID, n, *params.NotGiven)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found %d orders:\n", len(foundArr))
+	for i, order := range foundArr {
+		fmt.Printf("%d.\tid: %d\texpires: %s\n", i+1, order.ID, order.ExpireDate.Format("01.02.2006"))
+	}
+}
+
+func Return(serv service.Service, params parsing.Params) {
+	if params.ID == nil || params.RecipientID == nil {
+		fmt.Println("miss required flags")
+		return
+	}
+	err := serv.Return(*params.ID, *params.RecipientID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("order %d is returned successfully\n", *params.ID)
+}
+
+func ListReturn(serv service.Service, params parsing.Params) {
+	var (
+		pageNum, ordersPerPage int
+		err                    error
+	)
+	ordersPerPage = 10
+	if len(params.Args) > 0 {
+		pageNum, err = strconv.Atoi(params.Args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	if len(params.Args) > 1 {
+		ordersPerPage, err = strconv.Atoi(params.Args[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	arr, err := serv.ListReturn(pageNum, ordersPerPage)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	startPos := 1
+	if pageNum == 0 {
+		fmt.Println("all returned not removed orders:")
+	} else {
+		startPos = ordersPerPage*(pageNum-1) + 1
+		fmt.Printf("returned not removed orders from page %d (%d-%d):\n", pageNum, startPos, startPos+len(arr)-1)
+	}
+
+	for i, order := range arr {
+		fmt.Printf("%d.\tid: %d\trecipient: %d\texpires: %s\n", startPos+i, order.ID, order.RecipientID, order.ExpireDate.Format("01.02.2006"))
+	}
 }
--- Homework-2/cmd/main.go
+++ Homework-2/cmd/main.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 	"homework2/pup/cmd/command"
 	"homework2/pup/cmd/parsing"
-	"homework2/pup/internal/model"
 	"homework2/pup/internal/service"
 	"homework2/pup/internal/storage"
-	"strconv"
 )
 
 func main() {
@@ -27,117 +25,16 @@ func main() {
 	case "help":
 		command.Help()
 	case "accept":
-		if params.ID == nil || params.RecipientID == nil || params.ExpireString == nil {
-			fmt.Println("miss required flags")
-			return
-		}
-		err = serv.AcceptFromCourier(model.OrderInput{
-			ID:          *params.ID,
-			RecipientID: *params.RecipientID,
-			ExpireDate:  *params.ExpireString,
-		})
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println("got new order from courier")
+		command.Accept(serv, params)
 	case "remove":
-		if params.ID == nil || params.RecipientID == nil || params.ExpireString == nil {
-			fmt.Println("miss required flags")
-			return
-		}
-		err = serv.Remove(*params.ID)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("removed order %d from our pick-up point\n", *params.ID)
+		command.Remove(serv, params)
 	case "give":
-		if len(params.Args) == 0 {
-			fmt.Println("expected at least one argument as order id")
-			return
-		}
-		ids := make([]int64, len(params.Args))
-		for i, s := range params.Args {
-			idCur, err := strconv.ParseInt(s, 10, 64)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			ids[i] = idCur
-		}
-		err = serv.Give(ids)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println("all orders have been given to its recipient")
+		command.Give(serv, params)
 	case "list":
-		if params.RecipientID == nil {
-			fmt.Println("miss required flags")
-			return
-		}
-		var n int
-		if len(params.Args) > 0 {
-			n, err = strconv.Atoi(params.Args[0])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-		foundArr, err := serv.List(*params.RecipientID, n, *params.NotGiven)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("found %d orders:\n", len(foundArr))
-
-		for i, order := range foundArr {
-			fmt.Printf("%d.\tid: %d\texpires: %s\n", i+1, order.ID, order.ExpireDate.Format("01.02.2006"))
-		}
+		command.List(serv, params)
 	case "return":
-		if params.ID == nil || params.RecipientID == nil {
-			fmt.Println("miss required flags")
-			return
-		}
-		err = serv.Return(*params.ID, *params.RecipientID)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("order %d is returned successfully\n", *params.ID)
+		command.Return(serv, params)
 	case "list-return":
-		var pageNum int
-		ordersPerPage := 10
-		if len(params.Args) > 0 {
-			pageNum, err = strconv.Atoi(params.Args[0])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-		if len(params.Args) > 1 {
-			ordersPerPage, err = strconv.Atoi(params.Args[1])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-		arr, err := serv.ListReturn(pageNum, ordersPerPage)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		startPos := 1
-		if pageNum == 0 {
-			fmt.Println("all returned not removed orders:")
-		} else {
-			startPos = ordersPerPage*(pageNum-1) + 1
-			fmt.Printf("returned not removed orders from page %d (%d-%d):\n", pageNum, startPos, startPos+len(arr)-1)
-		}
-
-		for i, order := range arr {
-			fmt.Printf("%d.\tid: %d\trecipient: %d\texpires: %s\n", startPos+i, order.ID, order.RecipientID, order.ExpireDate.Format("01.02.2006"))
-		}
+		command.ListReturn(serv, params)
 	}
 }
