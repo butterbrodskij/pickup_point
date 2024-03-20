af02bd330c2ee0a8a17025e9c36c2f66a9d1abec
Kalimullin Marat <mer-marat@yandex.ru>
2024-03-20 16:12:47 +0300

    GOHW-3: Replace server logic from router to server

--- Homework-2/internal/api/router/router.go
+++ Homework-2/internal/api/router/router.go
@@ -2,15 +2,10 @@ package router
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"net/http"
-	"strconv"
 
 	"gitlab.ozon.dev/mer_marat/homework/internal/api/server"
-	"gitlab.ozon.dev/mer_marat/homework/internal/model"
 
 	"github.com/gorilla/mux"
 )
@@ -23,88 +18,22 @@ func MakeRouter(ctx context.Context, serv server.Server) *mux.Router {
 	router.HandleFunc("/pickpoint", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			var point model.PickPointAdd
-			if err = json.Unmarshal(body, &point); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			pointRepo := &model.PickPoint{
-				Name:    point.Name,
-				Address: point.Address,
-				Contact: point.Contact,
-			}
-			id, err := serv.Create(ctx, pointRepo)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			pointRepo.ID = id
-			pointJSON, _ := json.Marshal(pointRepo)
-			w.Write(pointJSON)
+			serv.Create(ctx, w, r)
 		case http.MethodPut:
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			var point model.PickPoint
-			if err = json.Unmarshal(body, &point); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			tag, err := serv.Update(ctx, &point)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Write(tag)
+			serv.Update(ctx, w, r)
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
 		}
 	})
 
 	router.HandleFunc(fmt.Sprintf("/pickpoint/{%s:[0-9]+}", queryParamKey), func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			vars := mux.Vars(r)
-			id, err := strconv.ParseInt(vars[queryParamKey], 10, 64)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			point, err := serv.Read(ctx, id)
-			if err != nil {
-				if errors.Is(err, model.ErrorObjectNotFound) {
-					w.WriteHeader(http.StatusNotFound)
-					return
-				}
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			pointJSON, _ := json.Marshal(point)
-			w.Write(pointJSON)
+			serv.Read(ctx, w, r)
 		case http.MethodDelete:
-			vars := mux.Vars(r)
-			id, err := strconv.ParseInt(vars[queryParamKey], 10, 64)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			tag, err := serv.Delete(ctx, id)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Write(tag)
+			serv.Delete(ctx, w, r)
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
 		}
 	})
 
--- Homework-2/internal/api/server/server.go
+++ Homework-2/internal/api/server/server.go
@@ -2,12 +2,19 @@ package server
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"strconv"
 
 	"gitlab.ozon.dev/mer_marat/homework/internal/model"
 	"gitlab.ozon.dev/mer_marat/homework/internal/pkg/repository/postgres"
 
-	"github.com/jackc/pgconn"
+	"github.com/gorilla/mux"
 )
+
+const queryParamKey = "point"
 
 type Server struct {
 	repo *postgres.PickpointRepo
@@ -17,18 +24,82 @@ func NewServer(repo *postgres.PickpointRepo) Server {
 	return Server{repo: repo}
 }
 
-func (s Server) Create(ctx context.Context, point *model.PickPoint) (int64, error) {
-	return s.repo.Add(ctx, point)
+func (s Server) Create(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var point model.PickPointAdd
+	if err = json.Unmarshal(body, &point); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	pointRepo := &model.PickPoint{
+		Name:    point.Name,
+		Address: point.Address,
+		Contact: point.Contact,
+	}
+	id, err := s.repo.Add(ctx, pointRepo)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	pointRepo.ID = id
+	pointJSON, _ := json.Marshal(pointRepo)
+	w.Write(pointJSON)
 }
 
-func (s Server) Update(ctx context.Context, point *model.PickPoint) (pgconn.CommandTag, error) {
-	return s.repo.Update(ctx, point)
+func (s Server) Update(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var point model.PickPoint
+	if err = json.Unmarshal(body, &point); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	tag, err := s.repo.Update(ctx, &point)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(tag)
 }
 
-func (s Server) Read(ctx context.Context, id int64) (*model.PickPoint, error) {
-	return s.repo.GetByID(ctx, id)
+func (s Server) Read(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars[queryParamKey], 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	point, err := s.repo.GetByID(ctx, id)
+	if err != nil {
+		if errors.Is(err, model.ErrorObjectNotFound) {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	pointJSON, _ := json.Marshal(point)
+	w.Write(pointJSON)
 }
 
-func (s Server) Delete(ctx context.Context, id int64) (pgconn.CommandTag, error) {
-	return s.repo.Delete(ctx, id)
+func (s Server) Delete(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars[queryParamKey], 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	tag, err := s.repo.Delete(ctx, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(tag)
 }
