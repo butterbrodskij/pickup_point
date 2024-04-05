f79784d9fe9ae7e90ae614568ad277a0b0e29cd2
Kalimullin Marat <mer-marat@yandex.ru>
2024-04-05 13:22:46 +0300

    GOHW-5: Add unit tests for update and read handlers

--- Homework-2/internal/api/handlers/pickpoint/handler_test.go
+++ Homework-2/internal/api/handlers/pickpoint/handler_test.go
@@ -141,3 +141,144 @@ func TestDelete(t *testing.T) {
 		})
 	})
 }
+
+func TestRead(t *testing.T) {
+	t.Parallel()
+	var (
+		ctx       = context.Background()
+		id        = int64(100)
+		invalidID = int64(0)
+	)
+	t.Run("smoke test", func(t *testing.T) {
+		t.Parallel()
+		s := setUp(t)
+		defer s.tearDown()
+		req, _ := http.NewRequestWithContext(ctx, "GET", "/pickpoint/100", strings.NewReader(""))
+		w := httptest.NewRecorder()
+		m := mux.NewRouter()
+		s.mockServ.EXPECT().Read(gomock.Any(), id).Return(fixture.PickPoint().Valid1().P(), nil)
+		m.HandleFunc("/pickpoint/{point:[0-9]+}", s.handl.Read)
+		m.ServeHTTP(w, req)
+
+		s.handl.Read(w, req)
+
+		require.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, w.Body.String(), `{"id":100,"name":"Chertanovo","address":"Chertanovskaya street, 8","contacts":"+7(999)888-77-66"}`)
+	})
+	t.Run("fail", func(t *testing.T) {
+		t.Parallel()
+		t.Run("bad request", func(t *testing.T) {
+			t.Parallel()
+			s := setUp(t)
+			defer s.tearDown()
+			req, _ := http.NewRequestWithContext(ctx, "GET", "/pickpoint/0", strings.NewReader(""))
+			w := httptest.NewRecorder()
+			m := mux.NewRouter()
+			s.mockServ.EXPECT().Read(gomock.Any(), invalidID).Return(nil, model.ErrorInvalidInput)
+			m.HandleFunc("/pickpoint/{point:[0-9]+}", s.handl.Read)
+			m.ServeHTTP(w, req)
+
+			s.handl.Read(w, req)
+
+			require.Equal(t, http.StatusBadRequest, w.Code)
+			assert.Equal(t, w.Body.String(), "")
+		})
+		t.Run("not found", func(t *testing.T) {
+			t.Parallel()
+			s := setUp(t)
+			defer s.tearDown()
+			req, _ := http.NewRequestWithContext(ctx, "GET", "/pickpoint/100", strings.NewReader(""))
+			w := httptest.NewRecorder()
+			m := mux.NewRouter()
+			s.mockServ.EXPECT().Read(gomock.Any(), id).Return(nil, model.ErrorObjectNotFound)
+			m.HandleFunc("/pickpoint/{point:[0-9]+}", s.handl.Read)
+			m.ServeHTTP(w, req)
+
+			s.handl.Read(w, req)
+
+			require.Equal(t, http.StatusNotFound, w.Code)
+			assert.Equal(t, w.Body.String(), "")
+		})
+		t.Run("internal error", func(t *testing.T) {
+			t.Parallel()
+			s := setUp(t)
+			defer s.tearDown()
+			req, _ := http.NewRequestWithContext(ctx, "GET", "/pickpoint/100", strings.NewReader(""))
+			w := httptest.NewRecorder()
+			m := mux.NewRouter()
+			s.mockServ.EXPECT().Read(gomock.Any(), id).Return(nil, assert.AnError)
+			m.HandleFunc("/pickpoint/{point:[0-9]+}", s.handl.Read)
+			m.ServeHTTP(w, req)
+
+			s.handl.Read(w, req)
+
+			require.Equal(t, http.StatusInternalServerError, w.Code)
+			assert.Equal(t, w.Body.String(), "")
+		})
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Parallel()
+	var (
+		ctx = context.Background()
+	)
+	t.Run("smoke test", func(t *testing.T) {
+		t.Parallel()
+		s := setUp(t)
+		defer s.tearDown()
+		body := `{"id":100, "name":"Chertanovo", "address":"Chertanovskaya street, 13", "contacts":"+7(999)888-77-66"}`
+		req, _ := http.NewRequestWithContext(ctx, "PUT", "/pickpoint", strings.NewReader(body))
+		w := httptest.NewRecorder()
+		s.mockServ.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
+
+		s.handl.Update(w, req)
+
+		require.Equal(t, w.Code, http.StatusOK)
+		assert.Equal(t, w.Body.String(), "operation completed successfully")
+	})
+	t.Run("fail", func(t *testing.T) {
+		t.Parallel()
+		t.Run("bad request", func(t *testing.T) {
+			t.Parallel()
+			s := setUp(t)
+			defer s.tearDown()
+			body := `example of bad request`
+			req, _ := http.NewRequestWithContext(ctx, "PUT", "/pickpoint", strings.NewReader(body))
+			w := httptest.NewRecorder()
+
+			s.handl.Update(w, req)
+
+			require.Equal(t, w.Code, http.StatusBadRequest)
+			assert.Equal(t, w.Body.String(), "")
+		})
+		t.Run("internal error", func(t *testing.T) {
+			t.Parallel()
+			s := setUp(t)
+			defer s.tearDown()
+			body := `{"id":100, "name":"Chertanovo", "address":"Chertanovskaya street, 13", "contacts":"+7(999)888-77-66"}`
+			req, _ := http.NewRequestWithContext(ctx, "PUT", "/pickpoint", strings.NewReader(body))
+			w := httptest.NewRecorder()
+			s.mockServ.EXPECT().Update(gomock.Any(), gomock.Any()).Return(assert.AnError)
+
+			s.handl.Update(w, req)
+
+			require.Equal(t, w.Code, http.StatusInternalServerError)
+			assert.Equal(t, w.Body.String(), "")
+		})
+		t.Run("not found", func(t *testing.T) {
+			t.Parallel()
+			s := setUp(t)
+			defer s.tearDown()
+			body := `{"id":100, "name":"Chertanovo", "address":"Chertanovskaya street, 13", "contacts":"+7(999)888-77-66"}`
+			req, _ := http.NewRequestWithContext(ctx, "PUT", "/pickpoint", strings.NewReader(body))
+			w := httptest.NewRecorder()
+			s.mockServ.EXPECT().Update(gomock.Any(), gomock.Any()).Return(model.ErrorObjectNotFound)
+
+			s.handl.Update(w, req)
+
+			require.Equal(t, w.Code, http.StatusNotFound)
+			assert.Equal(t, w.Body.String(), "")
+		})
+	})
+}
